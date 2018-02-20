64ece6b6b2248ef3e85ec5fdcdc454eaadf439a9
pesto93 <leonardpesto93@yahoo.com>
2018-02-20 14:49:54 +0200

    Account details

---
+++ src/pages/account-details/account-details.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AccountDetailsPage } from './account-details';
+import {TranslateModule} from "@ngx-translate/core";
+
+@NgModule({
+  declarations: [
+    AccountDetailsPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AccountDetailsPage),
+    TranslateModule.forChild()
+  ],
+  exports: [AccountDetailsPage]
+})
+export class AccountDetailsPageModule {}
